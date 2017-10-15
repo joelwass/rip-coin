65baf84ecbefc87a5d7b64ba15f9c32dc74e520c
Nathan Johnson <najohnson@redventures.com>
2017-10-14 20:37:19 -0400

    Adding vuex

--- rip-coin/src/main.js
+++ rip-coin/src/main.js
@@ -2,16 +2,25 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import Vuex from 'vuex';
 import '../node_modules/vuetify/dist/vuetify.min.css';
 import App from './App';
 import router from './router';
+import modules from './store';
 
 Vue.use(Vuetify);
+Vue.use(Vuex);
 Vue.config.productionTip = false;
+
+const store = new Vuex.Store({
+  modules,
+});
+
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   template: '<App/>',
   components: { App },
