8382b71f7ac3560aa33ebab89db141f3b682e484
Nathan Johnson <najohnson@redventures.com>
2017-11-01 17:26:36 -0400

    Adding hash of genesis block to IPFS

--- rip/chain.go
+++ rip/chain.go
@@ -1,5 +1,8 @@
 package rip
 
+// Genesis is the IPFS hash of the genesis block
+const Genesis = "QmbR1iGdnLWg3HaJtejJxw9TqPQkhquWYHUaStxHtezWih"
+
 // Block is a block in the chain
 type Block struct {
 	Transactions []Tx `json:"transactions"`
